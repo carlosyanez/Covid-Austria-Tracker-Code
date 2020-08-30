e48e1eccddccf65a431ef34bd5c87492a3a3f31b
carlosyanez <carlos.yanez.s@gmail.com>
2020-08-30 15:12:41 +0200

    added more stats

--- app.R
+++ app.R
@@ -22,7 +22,10 @@ ui <- fluidPage(theme = "bootstrap.css",
                 uiOutput("source_link"),
                 uiOutput("measurement_times")),
                 fluidRow(plotly::plotlyOutput("daily_active_plot")),
-                fluidRow(plotly::plotlyOutput("daily_new_plot"))
+                fluidRow(plotly::plotlyOutput("daily_new_plot")),
+                fluidRow(plotly::plotlyOutput("daily_age_plot")),
+                fluidRow(plotly::plotlyOutput("daily_positive_plot"))
+                
                 )
                 
 server <- function(input, output) {
@@ -32,9 +35,7 @@ server <- function(input, output) {
     rds_file <- "retrieved_data.rds"
     rds_url <- "https://github.com/carlosyanez/covid_austria_tracker/raw/master/retrieved_data.rds"
   
-
     download.file(rds_url,rds_file)
-
   
    retrieved_data <- readRDS(rds_file)
    
@@ -87,8 +88,61 @@ server <- function(input, output) {
      scale_y_continuous(labels=function(x) format(x, big.mark = ",", scientific = FALSE)) +
      scale_fill_tableau(breaks=(retrieved_data$retrieved_data %>% filter(!(State %in% c("Total"))) %>% pull(State) %>% unique(.)))
     
+   positive_plot <- retrieved_data$retrieved_data_general %>%
+     select(Date,positive_rate) %>%
+     ggplot(aes(x=Date,y=positive_rate)) + geom_line(color="pink") +
+     geom_point(color="red") +
+     theme_economist_white() + 
+     labs(title="Positive Rate",
+          x="Date",
+          y="Positive Rate (%)",
+          caption="Data: https://info.gesundheitsministerium.at" ) +
+     theme(legend.position = "bottom",
+           plot.title = element_text(size=14),
+           axis.title.x = element_text(size = 10),
+           axis.text.x = element_text(angle = 0, hjust = 1,size = 10),
+           axis.title.y = element_text(size = 10),
+           axis.text.y = element_text(size = 10),
+           strip.text.x = element_text(size = 12),
+           strip.text.y = element_text(size = 12, angle = 90),
+           legend.title=element_text(size=8),
+           legend.text=element_text(size=8)) +
+     scale_y_continuous(labels=function(x) format(x, big.mark = ",", scientific = FALSE)) 
+   
+   age_plot <- retrieved_data$retrieved_data_general %>%
+     select(-GesTestungen,-PositivGetestet,-positive_rate) %>%
+     pivot_longer(-Date) %>% filter(!(Date=="2020-06-29")) %>%
+     mutate(value=ifelse(value<0,0,value)) 
+   
+   f<- tibble(name=unique(age_plot$name), 
+              Group=factor(unique(age_plot$name),levels=c(">84","75-84","65-74","55-64",
+                                                          "45-54","35-44","25-34",
+                                                          "15-24","5-14","<5")))
+   
+   age_plot <-  age_plot  %>% left_join(f,by="name") %>%
+     select(Date,Group,value) %>%
+     ggplot(aes(x=Date,y=value,fill=Group)) + geom_bar(stat="identity") +
+     theme_economist_white() + 
+     labs(title="Cases per Age Group",
+          x="Date",
+          y="Cases",
+          caption="Data: https://info.gesundheitsministerium.at" ) +
+     theme(legend.position = "bottom",
+           plot.title = element_text(size=14),
+           axis.title.x = element_text(size = 10),
+           axis.text.x = element_text(angle = 0, hjust = 1,size = 10),
+           axis.title.y = element_text(size = 10),
+           axis.text.y = element_text(size = 10),
+           strip.text.x = element_text(size = 12),
+           strip.text.y = element_text(size = 12, angle = 90),
+           legend.title=element_text(size=8),
+           legend.text=element_text(size=8))+
+     scale_y_continuous(labels=function(x) format(x, big.mark = ",", scientific = FALSE)) 
+   
+   
     url <- a("Amtliches Dashboard COVID19 - Bundesministerium für Soziales, Gesundheit, Pflege und Konsumentenschutz",
              href="https://info.gesundheitsministerium.at/dashboard_Epidem.html?l=en")
+    
     
     
     output$source_link <- renderUI({
@@ -108,6 +162,14 @@ server <- function(input, output) {
     
     output$daily_new_plot <- plotly::renderPlotly({
         ggplotly(new_plot)
+    })
+    
+    output$daily_positive_plot <- plotly::renderPlotly({
+      ggplotly(positive_plot)
+    })
+    
+    output$daily_age_plot <- plotly::renderPlotly({
+      ggplotly(age_plot)
     })
 }
 
--- data_retriever.R
+++ data_retriever.R
@@ -13,15 +13,16 @@ library(ggthemes)
 
 setwd("/opt/shinyserver/apps/covid_vienna")
 special_flag <-1
-# find wd and set here
-file_path<-"./data/"
+file_path<-"/opt/shinyserver/apps/covid_vienna/data/"
 
 if(special_flag==1){
-      extract_filename <- c("Bundesland","Bezirke")
+      extract_filename <- c("Bundesland","Bezirke",
+                            "AllgemeinDaten","Altersverteilung",
+                            "GenesenTodesFaelleBL")
 
-      temp <- "./data.zip"
-
-      download.file("https://info.gesundheitsministerium.at/data/data.zip","temp")
+      temp <- "/opt/shinyserver/apps/covid_vienna/data.zip"
+      #temp <- "./data.zip"
+      download.file("https://info.gesundheitsministerium.at/data/data.zip",temp)
       unzip(temp,paste(extract_filename,".csv",sep=""),exdir=file_path)
 
       file.rename(paste(file_path,extract_filename,".csv",sep=""),
@@ -34,7 +35,7 @@ if(special_flag==1){
       rm(extract_filename)
 }
 
-
+####Retrieve data from csv files.
 
 file_list <- tibble(filename=list.files(path=file_path))
 
@@ -47,18 +48,23 @@ if(nrow(file_active)>1){
     active_data <-rbind(active_data,active_data_i)
     
   }
- # rm(active_data_i,file_list,file_path,i)
+  rm(active_data_i,i,file_active)
 }
 
 active_data <-  active_data  %>%
-  mutate(date=as.Date(as.character(substr(Timestamp,1,10),
+  mutate(Date=as.Date(as.character(substr(Timestamp,1,10),
                                    format = "%y-%m-%d"))) %>%
-                  select(date,Bundesland,Anzahl, GKZ)
+                  select(Date,Bundesland,Anzahl, GKZ)
 
-active_totals <- active_data %>% select(date,Anzahl) %>% group_by(date) %>% summarise(Anzahl=sum(Anzahl)) %>%
-  ungroup() %>% mutate(Bundesland="Total",GKZ="0")
+active_totals <- active_data %>% select(Date,Anzahl) %>%
+                  group_by(Date) %>% 
+                  summarise(Anzahl=sum(Anzahl)) %>%
+                   ungroup() %>% 
+                    mutate(Bundesland="Total",GKZ="0")
 
-active_data <- rbind(active_data,active_totals) %>% select(State=Bundesland,active=Anzahl,Date=date,GKZ=GKZ) 
+active_data <- rbind(active_data,active_totals) %>% 
+               select(State=Bundesland,active=Anzahl,Date=Date,GKZ=GKZ) 
+
 rm(active_totals)
 
 
@@ -71,24 +77,99 @@ if(nrow(file_cumul)>1){
     cumul_data <-rbind(cumul_data,cumul_data_i)
     
   }
-  # rm(cumul_data_i,file_list,file_path,i)
+   rm(cumul_data_i,i,file_cumul)
 }
 
 cumul_data <-  cumul_data  %>% 
-  mutate(date=as.Date(as.character(substr(Timestamp,1,10),
+               mutate(Date=as.Date(as.character(substr(Timestamp,1,10),
                                         format = "%y-%m-%d")),
          GKZ=floor(GKZ/100)) %>%
-  group_by(date,GKZ) %>% summarise(Anzahl=sum(Anzahl)) %>% ungroup()
+         group_by(Date,GKZ) %>% 
+         summarise(Anzahl=sum(Anzahl)) %>% 
+         ungroup()
 
-cumul_totals <- cumul_data %>% select(date,Anzahl) %>% group_by(date) %>% summarise(Anzahl=sum(Anzahl)) %>%
-  ungroup() %>% mutate(GKZ="0")
+cumul_totals <- cumul_data %>% select(Date,Anzahl) %>%
+                group_by(Date) %>% 
+                summarise(Anzahl=sum(Anzahl)) %>%
+                ungroup() %>% mutate(GKZ="0")
 
-cumul_data <- rbind(cumul_data,cumul_totals) %>% select(cumul=Anzahl,Date=date,GKZ=GKZ) 
+cumul_data <- rbind(cumul_data,cumul_totals) %>% select(cumul=Anzahl,Date=Date,GKZ=GKZ) 
 rm(cumul_totals)
 
-retrieved_data <-active_data %>%  left_join(cumul_data,by=c("Date","GKZ"))
-retrieved_data[is.na(retrieved_data$cumul),]$cumul <-0
 
+file_gt <- file_list %>% filter(grepl("GenesenTodesFaelleBL",filename))
+
+gt_data <- read.csv(paste(file_path,file_gt[1,]$filename,sep=""),sep=";")
+if(nrow(file_gt)>1){
+  for (i in 2:nrow(file_gt)){
+    gt_data_i <- read.csv(paste(file_path,file_gt[i,]$filename,sep=""),sep=";")
+    gt_data <-rbind(gt_data,gt_data_i)
+    
+  }
+  rm(gt_data_i,i,file_gt)
+}
+
+gt_data <-  gt_data  %>% 
+  mutate(Date=as.Date(as.character(substr(Timestamp,1,10),
+                                   format = "%y-%m-%d"))) %>%
+        select(-Timestamp)
+
+gt_totals <- gt_data %>% select(Date,Genesen,Todesfälle) %>%
+  group_by(Date) %>% 
+  summarise(Genesen=sum(Genesen),
+            Todesfälle=sum(Todesfälle)) %>%
+  ungroup() %>%  mutate(Bundesland="Total",GKZ="0")
+
+gt_data <- rbind(gt_data,gt_totals) %>%
+            mutate(GnT=Genesen+Todesfälle) %>%
+            select(GnT,Genesen,Todesfälle,Date=Date,GKZ=GKZ) 
+
+rm(gt_totals)
+
+
+file_ag <- file_list %>% filter(grepl("AllgemeinDaten",filename))
+
+ag_data <- read.csv(paste(file_path,file_ag[1,]$filename,sep=""),sep=";")
+if(nrow(file_ag)>1){
+  for (i in 2:nrow(file_ag)){
+    ag_data_i <- read.csv(paste(file_path,file_ag[i,]$filename,sep=""),sep=";")
+    ag_data <-rbind(ag_data,ag_data_i)
+    
+  }
+  rm(ag_data_i,i,file_ag)
+}
+
+ag_data <-  ag_data  %>% 
+  mutate(Date=as.Date(as.character(substr(Timestamp,1,10),
+                                   format = "%y-%m-%d"))) %>%
+  select(Date,PositivGetestet,GesTestungen)
+
+
+file_av <- file_list %>% filter(grepl("Altersverteilung",filename))
+
+av_data <- read.csv(paste(file_path,file_av[1,]$filename,sep=""),sep=";")
+if(nrow(file_av)>1){
+  for (i in 2:nrow(file_av)){
+    av_data_i <- read.csv(paste(file_path,file_av[i,]$filename,sep=""),sep=";")
+    av_data <-rbind(av_data,av_data_i)
+    
+  }
+  rm(av_data_i,i,file_av)
+}
+
+av_data <-  av_data  %>% 
+  mutate(Date=as.Date(as.character(substr(Timestamp,1,10),
+                                   format = "%y-%m-%d"))) %>%
+  pivot_wider(values_from = Anzahl, names_from=Altersgruppe) %>% select(-Timestamp)
+
+rm(file_list)
+
+####Process
+retrieved_data <-active_data %>% 
+                left_join(cumul_data,by=c("Date","GKZ")) %>%
+                left_join(gt_data,by=c("Date","GKZ"))
+
+retrieved_data[is.na(retrieved_data$cumul),]$cumul <-0
 
 dates <- tibble(Date=unique(cumul_data[order(cumul_data$Date),]$Date)) %>%
  mutate(previous_date=data.table::shift(Date)) 
@@ -103,12 +184,40 @@ retrieved_data <- retrieved_data %>%
   mutate(new_cases=ifelse(is.na(previous_date),0,cumul-previous_cumul)) %>%
   mutate(new_cases=ifelse(new_cases<0,0,new_cases))
 
+rm(active_data,cumul_data,gt_data,dates)
+
+retrieved_data_general <- ag_data %>%
+                          left_join(av_data,by="Date")
+
+dates <- tibble(Date=unique(ag_data[order(ag_data$Date),]$Date)) %>%
+  mutate(previous_date=data.table::shift(Date)) 
+
+
+retrieved_data_general <- retrieved_data_general %>% 
+  left_join(dates,by="Date") %>%
+  left_join(retrieved_data_general %>% 
+            mutate(previous_date=Date) %>% select(-Date),
+            by="previous_date") %>%
+  select(-previous_date)
+
+retrieved_data_general <- retrieved_data_general %>%
+  pivot_longer(cols = -Date) %>%
+  mutate(name = sub('*.[x,y]', '', name)) 
+
+retrieved_data_general[is.na(retrieved_data_general$value),]$value <-0
+
+retrieved_data_general <- retrieved_data_general %>% group_by(Date, name) %>%
+  summarise(diff = -diff(value)) %>% ungroup() %>%
+  pivot_wider(names_from = name, values_from = diff) %>%
+  mutate(positive_rate=100*PositivGetestet/GesTestungen)
+
+
 if(special_flag==1){
       covid_austria <-vector(mode = "list", length = 0)
 
       covid_austria$retrieved_data <- retrieved_data
+      covid_austria$retrieved_data_general <- retrieved_data_general
       covid_austria$timestamp <- now()
     saveRDS(covid_austria, file="retrieved_data.rds") 
 }
 
-system("ls")
